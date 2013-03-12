457ab2c43cb4384075894f4da87ca738d7a09dab
mo0rhuhn <max-schrimpf@hotmail.com>
2013-03-12 15:34:14 +0100

    db validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ActiveRecord::Base
   attr_accessible :available, :description, :image_url, :price, :title
+
+  validates_presence_of :title, :description, :price
+
+  validates_numericality_of :price
 end

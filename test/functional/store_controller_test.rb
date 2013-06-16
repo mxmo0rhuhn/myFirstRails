09ea15800b0add1e6a9fe3602ca7f985cac2aacc
mo0rhuhn <max-schrimpf@hotmail.com>
2013-06-16 17:38:58 +0200

    fixed strange test

--- test/functional/store_controller_test.rb
+++ test/functional/store_controller_test.rb
@@ -7,10 +7,4 @@ class StoreControllerTest < ActionController::TestCase
     assert_select '#main .entry', 1 # alles in meinen fixures
   end
 
-test "markup needed for store.js.coffee is in place" do
-get :index
-assert_select '.store .entry > img', 3
-assert_select '.entry input[type=submit]', 3
-end
-
 end
